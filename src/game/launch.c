8fa825a0ff204f1cd1bf1a449c1b85f9f98bf047
agatocherry <agcolas@student.42.fr>
2022-06-08 13:12:30 +0200

    ✨ (game): Add window

--- src/game/launch.c
+++ src/game/launch.c
@@ -6,7 +6,7 @@
 /*   By: agcolas <agcolas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 11:43:21 by agcolas           #+#    #+#             */
-/*   Updated: 2022/06/07 11:30:26 by agcolas          ###   ########.fr       */
+/*   Updated: 2022/06/08 13:10:56 by agcolas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 
 int	game_launch(t_file *file)
 {
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 640, 480, "Cub3d");
+	mlx_loop(mlx);
 	return (0);
 }
